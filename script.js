0f16e6d22dfc524c7352673d54eca5a0bbdeea67
Odmund Bautista <odfelip.bautista@gmail.com>
2024-01-17 16:22:04 -0600

    Cleanup of home page
    Nav works as intended on mobile devices (still need to do a little bit of testing).
    Next, finish the rest of the home page

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-bar');
+const main = document.querySelector('main');
 const navLinks = document.querySelectorAll('.nav-link');
 
 const navSlide = () => {
     // Toggle nav event
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        document.body.classList.toggle('nav-active');
         // Animate the links
         navLinks.forEach((link, index) => {
             // Index / 7 allows for a delay for the links to 'pop in'
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 + 0.3}s`;
                 console.log(index / 7);
             }
         });
